cf1f7468e9699cae1c56cf942971a85519ff3123
gxy <2946066704@qq.com>
2026-04-15 21:02:01 +0800

    mock引入和简单接口封装

--- vite.config.ts
+++ vite.config.ts
@@ -8,42 +8,49 @@ import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 //引入svg需要的插件
 import { createSvgIconsPlugin } from 'vite-plugin-svg-icons'
 import path from 'path'
+// 引入mock
+import { viteMockServe } from 'vite-plugin-mock'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [
-    vue(),
-    VueSetupExtend(),
-    AutoImport({
-      resolvers: [ElementPlusResolver()], // 自动导入 Element Plus API
-    }),
-    Components({
-      resolvers: [ElementPlusResolver()], // 自动导入 Element Plus 组件
-    }),
-    createSvgIconsPlugin({
-      // Specify the icon folder to be cached
-      iconDirs: [path.resolve(process.cwd(), 'src/assets/icons')],
-      // Specify symbolId format
-      symbolId: 'icon-[dir]-[name]',
-    }),
-  ],
-  server: {
-    // 核心配置：指定监听的地址为 127.0.0.1
-    host: '127.0.0.1',
-    // 可选：保留你原本的端口配置，默认是 5173
-    port: 5173,
-  },
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
+export default defineConfig(({ command }) => {
+  return {
+    plugins: [
+      vue(),
+      VueSetupExtend(),
+      AutoImport({
+        resolvers: [ElementPlusResolver()], // 自动导入 Element Plus API
+      }),
+      Components({
+        resolvers: [ElementPlusResolver()], // 自动导入 Element Plus 组件
+      }),
+      createSvgIconsPlugin({
+        // Specify the icon folder to be cached
+        iconDirs: [path.resolve(process.cwd(), 'src/assets/icons')],
+        // Specify symbolId format
+        symbolId: 'icon-[dir]-[name]',
+      }),
+      viteMockServe({
+        enable: command === 'serve',//mock配置
+      }),
+    ],
+    server: {
+      // 核心配置：指定监听的地址为 127.0.0.1
+      host: '127.0.0.1',
+      // 可选：保留你原本的端口配置，默认是 5173
+      port: 5173,
     },
-  },
-  //scss全局
-  css: {
-    preprocessorOptions: {
-      scss: {
-        additionalData: `@use "@/style/variable.scss" as *;`,
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url)),
       },
     },
-  },
+    //scss全局
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: `@use "@/style/variable.scss" as *;`,
+        },
+      },
+    },
+  }
 })
