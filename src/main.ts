aaa8fec1296e6bfa383c383b345942a3e77ad6f5
gxy <2946066704@qq.com>
2026-04-15 19:45:14 +0800

    完成环境配置

--- src/main.ts
+++ src/main.ts
@@ -6,8 +6,9 @@ import 'element-plus/dist/index.css'
 import zhCn from 'element-plus/es/locale/lang/zh-cn'
 
 const app = createApp(App)
-//加载以及设置中文
+//加载以及设置中文 
 app.use(ElementPlus, {
   locale: zhCn,
 })
 app.mount('#app')
+// console.log(import.meta.env);
