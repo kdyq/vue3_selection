d81431680a95900f5919f6a98c5a67793c2f6f6b
gxy <2946066704@qq.com>
2026-04-15 15:01:58 +0800

    src路径配置

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,12 @@
 import { createApp } from 'vue'
-import App from './App.vue'
+import App from '@/App.vue'
 //引入element-plus
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
 import zhCn from 'element-plus/es/locale/lang/zh-cn'
 
 const app = createApp(App)
-//加载以及设置中文 
+//加载以及设置中文
 app.use(ElementPlus, {
   locale: zhCn,
 })
