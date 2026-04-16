e263c21f4bdb0804696a2b7e2933c07b02192ca4
gxy <2946066704@qq.com>
2026-04-16 17:57:22 +0800

    登录字段部分ts类型设置

--- src/api/user/type.ts
+++ src/api/user/type.ts
@@ -5,17 +5,19 @@ export interface loginFormData {
   username: string
   password: string
 }
-
+interface dataType {
+  token?: string,
+  message?: string
+}
 // 定义全部接口返回数据都拥有 ts 类型
 export interface ResponseData {
   code: number
-  message: string
   ok: boolean
 }
 
 // 登录接口返回数据类型
 export interface loginResponseData extends ResponseData {
-  data: string
+  data: dataType
 }
 
 // 定义用户信息返回数据类型
--- src/store/modules/user.ts
+++ src/store/modules/user.ts
@@ -1,20 +1,21 @@
 //用户相关信息的仓库
 import { defineStore } from 'pinia'
-import type { loginFormData } from '@/api/user/type'
+import type { loginFormData, loginResponseData } from '@/api/user/type'
 import { reqLogin } from '@/api/user'
 import { ref } from 'vue'
+import type { UserState } from './types/type'
 export const useUserStore = defineStore(
   'User',
   () => {
     const token = ref<string>('')
     const userLogin = async (data: loginFormData) => {
-      const result: any = await reqLogin(data)
+      const result: loginResponseData = await reqLogin(data)
       // console.log(result);
       if (result.code == 200) {
-        token.value = result.data.token
+        token.value = result.data.token as string
         return 'ok'
       } else {
-        return Promise.reject(new Error(result.data))
+        return Promise.reject(new Error(result.data.message))
       }
     }
     return { userLogin, token }
@@ -25,6 +26,10 @@ export const useUserStore = defineStore(
       key: 'token',
       storage: localStorage,
       paths: ['token'],
+      serializer: {
+        serialize: (state) => state.token,
+        deserialize: (value) => ({ token: value }),
+      },
     },
   },
 )
