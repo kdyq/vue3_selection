540a421c470d51bc0fe4b423483c54c1a725d16a
gxy <2946066704@qq.com>
2026-04-22 15:42:33 +0800

    属性管理静态搭建

---
+++ src/components/Category/index.vue
@@ -0,0 +1,47 @@
+<template>
+    <el-card>
+        <el-form :inline="true">
+            <el-form-item label="一级分类">
+                <el-select class="select-width">
+                    <el-option label="北京" value="beijing">
+                    </el-option>
+                    <el-option label="上海" value="shanghai">
+                    </el-option>
+                    <el-option label="深圳" value="shenzhen">
+                    </el-option>
+                </el-select>
+            </el-form-item>
+            <el-form-item label="二级分类">
+                <el-select class="select-width">
+                    <el-option label="北京" value="beijing">
+                    </el-option>
+                    <el-option label="上海" value="shanghai">
+                    </el-option>
+                    <el-option label="深圳" value="shenzhen">
+                    </el-option>
+                </el-select>
+            </el-form-item>
+            <el-form-item label="三级分类">
+                <el-select class="select-width">
+                    <el-option label="北京" value="beijing">
+                    </el-option>
+                    <el-option label="上海" value="shanghai">
+                    </el-option>
+                    <el-option label="深圳" value="shenzhen">
+                    </el-option>
+                </el-select>
+            </el-form-item>
+        </el-form>
+    </el-card>
+
+</template>
+
+<script setup lang="ts" name='category'>
+
+</script>
+
+<style scoped lang="scss">
+.select-width {
+    width: 220px;
+}
+</style>
