cc052f332dcd1658b236c29a89260b0ba6d8ed86
gxy <2946066704@qq.com>
2026-05-10 20:59:50 +0800

    SKU的详情查看的静态搭建

--- src/views/product/sku/index.vue
+++ src/views/product/sku/index.vue
@@ -22,7 +22,7 @@
                         :icon="row.isSale == 1 ? 'Bottom' : 'Top'" :title="row.isSale == 1 ? '下架' : '上架'"
                         @click="changeSale(row)"></el-button>
                     <el-button type="warning" size="default" icon="Edit" title="更新" @click="updateSku"></el-button>
-                    <el-button type="success" size="default" icon="View" title="查看"></el-button>
+                    <el-button type="success" size="default" icon="View" title="查看" @click="viewSku"></el-button>
                     <el-button type="danger" size="default" icon="Delete" title="删除"></el-button>
                 </template>
             </el-table-column>
@@ -31,6 +31,48 @@
         <el-pagination v-model:current-page="currentPage" v-model:page-size="pageSize" :page-sizes="[6, 8, 10]"
             :background="true" layout="prev, pager, next, jumper,->,sizes,total" :total="total" :pager-count="5"
             @current-change="getHasSku" @size-change="changePageSize" />
+        <!-- 抽屉组件，展示商品详情 -->
+        <el-drawer v-model="drawer">
+            <template #header>
+                <h4>SKU详情</h4>
+            </template>
+            <template #default>
+                <el-row style="margin-top: 10px;">
+                    <el-col :span="6">名称</el-col>
+                    <el-col :span="18">华为</el-col>
+                </el-row>
+                <el-row style="margin-top: 10px;">
+                    <el-col :span="6">描述</el-col>
+                    <el-col :span="18">华为</el-col>
+                </el-row>
+                <el-row style="margin-top: 10px;">
+                    <el-col :span="6">价格</el-col>
+                    <el-col :span="18">6999</el-col>
+                </el-row>
+                <el-row style="margin-top: 10px;">
+                    <el-col :span="6">平台属性</el-col>
+                    <el-col :span="18">
+                        <el-tag type="danger" v-for="i in 10" style="margin: 5px;">{{ i }}</el-tag>
+                    </el-col>
+                </el-row>
+                <el-row style="margin-top: 10px;">
+                    <el-col :span="6">销售属性</el-col>
+                    <el-col :span="18">
+                        <el-tag v-for="i in 10" style="margin: 5px;">{{ i }}</el-tag>
+                    </el-col>
+                </el-row>
+                <el-row style="margin-top: 10px;">
+                    <el-col :span="6">商品图片</el-col>
+                    <el-col :span="18">
+                        <el-carousel :interval="4000" type="card" height="200px">
+                            <el-carousel-item v-for="item in 6" :key="item">
+                                <h3 text="2xl" justify="center">{{ item }}</h3>
+                            </el-carousel-item>
+                        </el-carousel>
+                    </el-col>
+                </el-row>
+            </template>
+        </el-drawer>
 
     </el-card>
 
@@ -49,6 +91,8 @@ const pageSize = ref<number>(6);
 const total = ref<number>(0);
 //存储数据
 const skuArr = ref<SkuData[]>([])
+//控制抽屉组件的显示与隐藏
+const drawer = ref<boolean>(false)
 //组件挂载完毕
 onMounted(() => {
     getHasSku();
@@ -83,6 +127,26 @@ const changeSale = async (row: SkuData) => {
 const updateSku = () => {
     ElMessage.warning('此功能正在开发中...')
 }
+//查看商品详情
+const viewSku = () => {
+    drawer.value = true
+}
 </script>
 
-<style scoped></style>
+<style scoped>
+.el-carousel__item h3 {
+    color: #475669;
+    opacity: 0.75;
+    line-height: 200px;
+    margin: 0;
+    text-align: center;
+}
+
+.el-carousel__item:nth-child(2n) {
+    background-color: #99a9bf;
+}
+
+.el-carousel__item:nth-child(2n + 1) {
+    background-color: #d3dce6;
+}
+</style>
