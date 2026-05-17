24c06737d94861aff93cf9f3e0176fefbcd7e821
gxy <2946066704@qq.com>
2026-05-17 15:53:15 +0800

    菜单管理-添加与编辑菜单静态搭建

--- src/views/acl/permission/index.vue
+++ src/views/acl/permission/index.vue
@@ -6,10 +6,12 @@
             <el-table-column label="修改时间" prop="updateTime" />
             <el-table-column label="操作">
                 <template v-slot="{ row }">
-                    <el-button type="primary" size="small" icon="User" :disabled="row.level === 4 ? true : false">
+                    <el-button type="primary" size="small" icon="User" :disabled="row.level === 4 ? true : false"
+                        @click="addPermission">
                         {{ row.level === 3 ? '添加功能' : '添加菜单' }}
                     </el-button>
-                    <el-button type="warning" size="small" icon="Edit" :disabled="row.level === 1 ? true : false">
+                    <el-button type="warning" size="small" icon="Edit" :disabled="row.level === 1 ? true : false"
+                        @click="editPermission(row)">
                         编辑
                     </el-button>
                     <el-popconfirm title="确定删除这个吗" width="260px">
@@ -21,15 +23,32 @@
                 </template>
             </el-table-column>
         </el-table>
+        <!-- 添加或编辑的dialog组件 -->
+        <el-dialog v-model="dialogVisible" title="添加" width="30%">
+            <el-form style="margin-top: 30px;" label-width="80px">
+                <el-form-item label="名称：">
+                    <el-input placeholder="请输入名称"></el-input>
+                </el-form-item>
+                <el-form-item label="权限值：">
+                    <el-input placeholder="请输入权限值"></el-input>
+                </el-form-item>
+            </el-form>
+            <template #footer>
+                <el-button size="default" @click="dialogVisible = false">取消</el-button>
+                <el-button type="primary" size="default" @click="save">保存</el-button>
+            </template>
+        </el-dialog>
     </div>
 </template>
 
 <script setup lang="ts" name="permission">
 import { ref, onMounted } from 'vue'
 import { reqAllPermisson } from '@/api/acl/menu/index'
-import type { PermissionResponsData, PermissionList } from '@/api/acl/menu/type';
+import type { PermissionResponsData, PermissionList, Permission } from '@/api/acl/menu/type';
 //存储已有的菜单数据
 const permissionArr = ref<PermissionList>([])
+//控制添加或修改的dialog组件的显示与隐藏
+const dialogVisible = ref(false)
 onMounted(() => {
     getHasPermission()
 })
@@ -39,6 +58,18 @@ const getHasPermission = async () => {
     if (result.code === 200) {
         permissionArr.value = result.data
     }
+}
+//添加
+const addPermission = () => {
+    dialogVisible.value = true
+}
+//编辑
+const editPermission = (row: Permission) => {
+    dialogVisible.value = true
+}
+//保存菜单
+const save = () => {
+    dialogVisible.value = false
 }
 </script>
 
