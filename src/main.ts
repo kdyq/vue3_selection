cad314c3d87e888efcd46ef5f3b90c8054065ef2
gxy <2946066704@qq.com>
2026-05-17 20:58:43 +0800

    暗黑模式切换完成

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,8 @@ import '@/style/index.scss'
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
 import zhCn from 'element-plus/es/locale/lang/zh-cn'
+//暗黑模式样式
+import 'element-plus/theme-chalk/dark/css-vars.css'
 //svg配置
 import 'virtual:svg-icons-register'
 //引入自定义插件对象：注册全局组件
--- src/views/layout/tabbar/setbutton/index.vue
+++ src/views/layout/tabbar/setbutton/index.vue
@@ -6,9 +6,22 @@
     <el-tooltip content="全屏" placement="bottom">
         <el-button size="default" icon="FullScreen" circle @click="fullScreen"></el-button>
     </el-tooltip>
-    <el-tooltip content="设置" placement="bottom">
-        <el-button size="default" icon="Setting" circle></el-button>
-    </el-tooltip>
+    <el-popover placement="bottom" title="主题设置" :width="300" trigger="click">
+        <!-- 表单元素 -->
+        <el-form label-width="80px">
+            <el-form-item label="主题颜色">
+                <el-color-picker v-model="color" size="small" show-alpha :predefine="predefineColors" />
+            </el-form-item>
+            <el-form-item label="暗黑模式">
+                <el-switch @change="changeDark" v-model="dark" size="default" active-icon="MoonNight"
+                    inactive-icon="Sunny" inline-prompt />
+            </el-form-item>
+        </el-form>
+
+        <template #reference>
+            <el-button size="default" icon="Setting" circle title="设置"></el-button>
+        </template>
+    </el-popover>
     <img :src="userStore.avatar ? userStore.avatar : '../../../../../public/touxiang.png'"
         style="width: 32px; height: 32px; margin: 0 10px; border-radius: 50%;">
     <!-- 下拉菜单 -->
@@ -31,9 +44,30 @@
 import { useLayoutStore } from '@/store/modules/layout';
 import { useUserStore } from '@/store/modules/user';
 import { useRouter } from 'vue-router';
+import { ref } from 'vue'
 const router = useRouter();
 const userStore = useUserStore();
 const layoutStore = useLayoutStore();
+//颜色数据
+const color = ref('rgba(255, 69, 0, 0.68)')
+const predefineColors = ref([
+    '#ff4500',
+    '#ff8c00',
+    '#ffd700',
+    '#90ee90',
+    '#00ced1',
+    '#1e90ff',
+    '#c71585',
+    'rgba(255, 69, 0, 0.68)',
+    'rgb(255, 120, 0)',
+    'hsv(51, 100, 98)',
+    'hsva(120, 40, 94, 0.5)',
+    'hsl(181, 100%, 37%)',
+    'hsla(209, 100%, 56%, 0.73)',
+    '#c7158577',
+])
+// 主题设置
+const dark = ref(false)
 // 刷新
 const changeRefresh = () => {
     layoutStore.refresh = !layoutStore.refresh;
@@ -56,7 +90,13 @@ const logut = async () => {
     //跳转到登录页
     router.replace('/login')
 };
-
+//暗黑模式
+const changeDark = () => {
+    //获取html根节点
+    const html = document.documentElement
+    //根据dark添加类名
+    dark.value ? html.className = 'dark' : html.className = ''
+}
 </script>
 
 <style lang="scss" scoped></style>
